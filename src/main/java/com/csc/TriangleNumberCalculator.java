548099d8ef1a28df45197c85c914d3d0f3d9bfb4
jgraessle <j_graessle@mail.harpercollege.edu>
2024-10-20 20:07:06 -0700

    New Triangle Calculator Class and Tests
    The TriangleNumberCalculator class contains several methods that calculate triangular numbers using a recursive function value(int n) and either return, add, or subtract them. There are also Junit tests to test these functions using various numbers.

--- src/main/java/com/csc/TriangleNumberCalculator.java
+++ src/main/java/com/csc/TriangleNumberCalculator.java
@@ -1,5 +1,30 @@
 package com.csc;
 
 public class TriangleNumberCalculator {
-  // Your code goes here!
+  int triangle = 0;
+  int addTriangle = 0;
+  int subTriangle = 0;
+  public int value(int n) {
+    if (n <=1) {
+      return n;
+    }
+    return n + value(n-1);
+  }  
+  public int add(int n, int m) {
+    int tri1 = value(n);
+    int tri2 = value(m);
+    return tri1 + tri2;
+  }
+  public int subtract(int n, int m) {
+    int tri1 = value(n);
+    int tri2 = value(m);
+    return tri1 - tri2;
+  }
+  public static void main (String[] args) {
+    TriangleNumberCalculator calculator = new TriangleNumberCalculator();
+    System.out.println(calculator.value(13798));
+    System.out.println(calculator.add(14200, 100));
+    System.out.println(calculator.subtract(100, 100));
+    
+  }
 }
--- src/test/java/com/csc/TestTriangleNumberCalculator.java
+++ src/test/java/com/csc/TestTriangleNumberCalculator.java
@@ -1,6 +1,9 @@
 package com.csc;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 public class TestTriangleNumberCalculator {
 
@@ -11,5 +14,40 @@ public class TestTriangleNumberCalculator {
     calculator = new TriangleNumberCalculator();
   }
 
-  // Your tests go here
+  @Test
+  void TestValue1() {
+    assertEquals(1, calculator.value(1));
+  }
+  @Test
+  void TestValue2() {
+    assertEquals(3, calculator.value(2));
+  }
+  @Test
+  void TestValue4() {
+    assertEquals(10, calculator.value(4));
+  }
+  @Test
+  void TestAdd1N1() {
+    assertEquals(2, calculator.add(1, 1));
+  }
+  @Test
+  void TestAdd2N3() {
+    assertEquals(9, calculator.add(2, 3));
+  }
+  @Test
+  void TestAdd4N2() {
+    assertEquals(13, calculator.add(4, 2));
+  }
+  @Test
+  void TestSubract1N1() {
+    assertEquals(0, calculator.subtract(1, 1));
+  }
+  @Test
+  void TestSubtract2N3() {
+    assertEquals(-3, calculator.subtract(2, 3));
+  }
+  @Test
+  void TestSubtract4N2() {
+    assertEquals(7, calculator.subtract(4, 2));
+  }
 }
